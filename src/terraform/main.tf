5701f3c27c96175f1179520c6895503d37cb2b9d
Ghaby-X <gabrielanyaele21@gmail.com>
2024-11-09 13:31:36 +0000

    setup terraform

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -22,20 +22,6 @@ provider "azurerm" {
 
 }
 
-# --------------
-# variable definitions
-variable "resource_group_name" {
-  default = "rg-resumecloudchallenge-dev"
-}
-variable "resource_group_location" {
-  default = "eastus"
-}
-
-variable "project_name" {
-  default = "resumecloudchallenge"
-}
-
-
 #------------------
 # resource definitions
 resource "azurerm_resource_group" "rg" {
@@ -54,10 +40,9 @@ resource "random_string" "resource_code" {
 resource "azurerm_storage_account" "sa" {
   name                     = "sacloud${random_string.resource_code.result}"
   resource_group_name      = azurerm_resource_group.rg.name
-  location                 = "Canada Central"
+  location                 = var.resource_group_location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  account_kind             = "StorageV2"
 
   static_website {
     index_document     = "index.html"
@@ -73,57 +58,46 @@ resource "azurerm_storage_table" "visitorcount" {
 resource "azurerm_application_insights" "app-insights" {
   name                = "fa-app-insights-${var.project_name}"
   resource_group_name = azurerm_resource_group.rg.name
-  location            = "Canada Central"
+  location            = var.resource_group_location
   application_type    = "web"
 }
 
-resource "azurerm_app_service_plan" "asp" {
+resource "azurerm_service_plan" "asp" {
   name                = "asp-${var.project_name}"
-  location            = "Canada Central"
+  location            = var.resource_group_location
   resource_group_name = azurerm_resource_group.rg.name
-  kind                = "Linux"
+  os_type             = "Linux"
   reserved            = true
-
-  sku {
-    tier = "Dynamic"
-    size = "Y1"
-  }
-
-  lifecycle {
-    ignore_changes = [
-      kind
-    ]
-  }
+  sku_name            = "Y1"
 }
 
 
 resource "azurerm_function_app" "fa" {
   name                = "fa-${var.project_name}"
-  location            = "Canada Central"
+  location            = var.resource_group_location
   resource_group_name = azurerm_resource_group.rg.name
 
-  app_service_plan_id        = azurerm_app_service_plan.asp.id
+  app_service_plan_id        = azurerm_service_plan.asp.id
   storage_account_name       = azurerm_storage_account.sa.name
   storage_account_access_key = azurerm_storage_account.sa.primary_access_key
   os_type                    = "linux"
   version                    = "~4"
 
   app_settings = {
-    FUNCTIONS_WORKER_RUNTIME                 = "python"
-    APPINSIGHTS_INSTRUMENTATIONKEY           = azurerm_application_insights.app-insights.instrumentation_key
-    APPLICATIONINSIGHTS_CONNECTION_STRING    = azurerm_application_insights.app-insights.connection_string
-    WEBSITE_CONTENTAZUREFILECONNECTIONSTRING = "${azurerm_storage_account.sa.primary_connection_string}"
-    AzureWebJobsFeatureFlags                 = "EnableWorkerIndexing"
-    AzureWebJobsStorage                      = azurerm_storage_account.sa.primary_connection_string
-    WEBSITE_RUN_FROM_PACKAGE                 = "1"
-    CONNECTION_STRING                        = azurerm_storage_account.sa.primary_connection_string
+    FUNCTIONS_WORKER_RUNTIME              = "python"
+    APPINSIGHTS_INSTRUMENTATIONKEY        = azurerm_application_insights.app-insights.instrumentation_key
+    WEBSITE_RUN_FROM_PACKAGE              = "1"
+    APPLICATIONINSIGHTS_CONNECTION_STRING = azurerm_application_insights.app-insights.connection_string
+    AzureWebJobsFeatureFlags              = "EnableWorkerIndexing"
+    AzureWebJobsStorage                   = azurerm_storage_account.sa.primary_connection_string
+    CONNECTION_STRING                     = azurerm_storage_account.sa.primary_connection_string
   }
 
   site_config {
     linux_fx_version = "python|3.9" # remember to update this to 3.11
 
     cors {
-      allowed_origins     = [trimsuffix(azurerm_storage_account.sa.primary_web_endpoint, "/")]
+      allowed_origins     = ["https://portal.azure.com", trimsuffix(azurerm_storage_account.sa.primary_web_endpoint, "/")]
       support_credentials = true
     }
 
---
+++ src/terraform/variables.tf
@@ -0,0 +1,12 @@
+# variable definitions
+variable "resource_group_name" {
+  default = "rg-resumecloudchallenge-dev"
+}
+
+variable "resource_group_location" {
+  default = "Canada Central"
+}
+
+variable "project_name" {
+  default = "resumecloudchallenge"
+}
