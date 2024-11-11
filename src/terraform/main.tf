866f19a206007cc7f2a0e7fb095fe18c75b65f38
Ghaby-X <gabrielanyaele21@gmail.com>
2024-11-11 15:41:12 +0000

    fix: site not found

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -116,10 +116,8 @@ resource "azurerm_linux_function_app" "fa" {
   storage_account_access_key = azurerm_storage_account.sa.primary_access_key
 
   app_settings = {
-    SCM_DO_BUILD_DURING_DEPLOYMENT           = true
-    CONNECTION_STRING                        = azurerm_storage_account.sa.primary_connection_string
-    WEBSITE_CONTENTAZUREFILECONNECTIONSTRING = azurerm_storage_account.sa.primary_connection_string
-    WEBSITE_CONTENTSHARE                     = "f${var.project_name}"
+    SCM_DO_BUILD_DURING_DEPLOYMENT = true
+    CONNECTION_STRING              = azurerm_storage_account.sa.primary_connection_string
   }
 
   site_config {
